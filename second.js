b510d3d3142f6487d3e8d1de7a097b4286b7f1b8
v1cky007 <v1gneashm007@gmail.com>
2022-12-08 15:54:09 +0530

    Initial commit

---
+++ second.js
@@ -0,0 +1,17 @@
+console.log("Royan")
+document.title="Royan"
+alert("boring sleeping varshan");
+
+
+var name="Nithin"
+function abron(r){
+        console.log(r)
+        document.write(r)
+      }
+
+abron("Abrohneel")
+
+console.log(name)
+document.write(name)
+
+document.get
